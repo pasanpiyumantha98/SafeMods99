cd8bf36a2ebc77f8cda0abf8100826d2906116b9
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-08-19 14:12:32 +0530

    o1

--- SafeMods-Frontend/src/main.jsx
+++ SafeMods-Frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './assets/pages/home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Contact from './assets/pages/contact'
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Home />
-  </StrictMode>,
+  <BrowserRouter>
+  <Routes>
+
+  <Route path="/" element={<Home/>}/>
+  <Route path="/contact" element={<Contact/>}/>
+
+
+  </Routes>
+    
+  </BrowserRouter>,
 )
