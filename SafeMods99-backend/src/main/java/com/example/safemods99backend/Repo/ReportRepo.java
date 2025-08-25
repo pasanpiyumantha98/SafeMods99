31194add07dac4a6b4d0e2201f313e6d293c0ac9
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-08-25 21:17:05 +0530

    Update ReportRepo.java

--- SafeMods99-backend/src/main/java/com/example/safemods99backend/Repo/ReportRepo.java
+++ SafeMods99-backend/src/main/java/com/example/safemods99backend/Repo/ReportRepo.java
@@ -1,8 +1,9 @@
 package com.example.safemods99backend.Repo;
 
-import com.example.safemods99backend.Dto.ReportDto;
+
+import com.example.safemods99backend.Model.Report;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ReportRepo extends MongoRepository<ReportDto, Integer> {
+public interface ReportRepo extends MongoRepository<Report, Integer> {
 
 }
